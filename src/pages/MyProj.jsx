f2304db658a5eb89e882d3c00ba24cad6d954959
Asadbek Tursunaliyev <madrahimoffasadbek@gmail.com>
2025-09-22 21:24:06 +0500

    projects-v2

--- src/pages/MyProj.jsx
+++ src/pages/MyProj.jsx
@@ -4,13 +4,13 @@ import { FiLink } from "react-icons/fi";
 
 const projects = [
   {
-    name: `"Grasby" coming soon...`,
-    desc: "The largest online market in Uzbekistan.",
-    url: "#",
+    name: "Healthy Recipes",
+    desc: "Using ReactJS technology with TailwindCSS. You can add and delete your own healthy foods' recipes.",
+    url: "https://healthy-recipes-five.vercel.app",
   },
   {
-    name: "Coming soon...",
-    desc: "Description...",
+    name: `"Grasby" coming soon...`,
+    desc: "The largest online market in Uzbekistan.",
     url: "#",
   },
 ];
@@ -27,9 +27,9 @@ export default function MyProj() {
             <h2 className="text-base sm:text-lg md:text-xl font-semibold text-gray-800">
               {item.name}
             </h2>
-            <p className="text-sm sm:text-base text-gray-600">{item.desc}</p>
+            <p className="text-sm sm:text-base text-gray-600 line-clamp-2">{item.desc}</p>
             <Link
-              to={item.url}
+              to={item.url} target="_blank"
               className="inline-flex items-center gap-2 px-3 py-2 sm:px-4 sm:py-2 rounded bg-[#F1F5F9] hover:bg-[#F4F7FA] text-sm sm:text-base font-medium text-gray-800 shadow-sm transition"
             >
               <FiLink className="h-4 w-4" />
